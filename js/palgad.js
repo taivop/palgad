060b37e80c910195e94984294360c3d46bfcc46e
taivop <taivo.pungas@gmail.com>
2015-08-20 23:42:26 +0300

    example works but cant hide some features

--- js/palgad.js
+++ js/palgad.js
@@ -1,13 +1,29 @@
-console.log(d3.select("#chart"))
+toHide = ['', 'Tüüp', 'Asutus']
+
 
 var chart = c3.generate({
-    bindto: '#chart',
     data: {
-        columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-        ]
+        x : 'Inimesi',
+        ys : {
+            Mediaan: 'Mediaan',
+            Keskmine: 'Keskmine'
+        },
+        hide: toHide,
+        legend: {
+            hide: toHide
+        },
+        url: 'data/asutuse_kaupa.csv',
+        type: 'scatter'
+    },
+    axis: {
+        x: {
+            label: 'Inimesi',
+            tick: {
+                fit: false
+            }
+        },
+        y: {
+            label: 'Palk'
+        }
     }
 });
-
-console.log(chart);
