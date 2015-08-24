7dfe149973e2fa93eee53a17f0237ef9e42be894
taivop <taivo.pungas@gmail.com>
2015-08-25 00:29:09 +0300

    flare generation works as it needs to

--- analysis/generate_flare.R
+++ analysis/generate_flare.R
@@ -7,23 +7,40 @@ library(rjson)
 palgad_riik <- read_excel("data/palgad_riik.xlsx",
                           col_types=c(rep("text", 6), "numeric", "numeric"))
 
-root <- list() # will start filling this
-root$name <- "Riigiasutused"
-root$children <- character()
+make_node = function(root_name, children, add_size) {
+  root <- list()
+  root$name <- root_name
+  root$children <- children
+  
+  if(!missing(add_size) && add_size)
+    root$size <- 1000
+  
+  return(root)
+}
+
+to_list = function(asutused, root_name) {
+  l <- list() # will start filling this
+  
+  for(i in 1:nrow(asutused)) {
+    asutus <- asutused[i,]
+    
+    asutus_obj <- list()
+    asutus_obj$size <- asutus$palgasumma
+    asutus_obj$name <- asutus$Asutus
+    
+    l <- c(l, list(asutus_obj))
+  }
+  
+  return(l)
+}
 
 asutused <- palgad_riik %>%
   group_by(Asutus) %>%
   summarise(inimesi=n(), palgasumma=sum(Põhipalk)) %>%
   arrange(desc(palgasumma))
 
-for(i in 1:nrow(asutused)) {
-  asutus <- asutused[i,]
-
-  asutus_obj <- list()
-  asutus_obj$size <- asutus$palgasumma
-  asutus_obj$name <- asutus$Asutus
-  
-  root$children <- c(root$children, list(asutus_obj))
-}
+root <- make_node("Riigiasutused", list())
+root$children <- c(root$children, to_list(asutused[1:3,]))
+root$children <- c(root$children, list(make_node("alam", to_list(asutused[4:6,]), TRUE)))
 
 cat(toJSON(root), file="../data/flare2.json")
