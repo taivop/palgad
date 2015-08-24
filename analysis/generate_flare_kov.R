7c4ba3cb8ed73a2f5257bb462282cbf8fcdd7314
taivop <taivo.pungas@gmail.com>
2015-08-25 01:11:23 +0300

    kov bubble chart looks really good

--- analysis/generate_flare_kov.R
+++ analysis/generate_flare_kov.R
@@ -4,7 +4,7 @@ library(readxl)
 library(jsonlite)
 library(rjson)
 
-palgad_riik <- read_excel("data/palgad_riik.xlsx",
+palgad <- read_excel("data/palgad_kov.xlsx",
                           col_types=c(rep("text", 6), "numeric", "numeric"))
 
 make_node = function(root_name, children) {
@@ -22,7 +22,7 @@ to_list = function(asutused, root_name) {
     asutus <- asutused[i,]
     
     asutus_obj <- list()
-    asutus_obj$size <- asutus$palgasumma
+    asutus_obj$size <- round(asutus$palgasumma)
     asutus_obj$name <- asutus$Asutus
     
     l <- c(l, list(asutus_obj))
@@ -31,22 +31,18 @@ to_list = function(asutused, root_name) {
   return(l)
 }
 
-asutused <- palgad_riik %>%
+asutused <- palgad %>%
   group_by(Asutus) %>%
   summarise(inimesi=n(), palgasumma=sum(Põhipalk)) %>%
-  arrange(desc(palgasumma))
+  arrange(desc(palgasumma)) %>%
+  mutate(Asutus=gsub("Linnavalitsus", "linn", Asutus)) %>%
+  mutate(Asutus=gsub("Vallavalitsus", "vald", Asutus))
 
 step_size <- 20
 
 root <- make_node("Riigiasutused", list())
-root$children <- c(root$children, to_list(asutused[1:step_size,]))
+root$children <- c(root$children, to_list(asutused))
 
-for(i in seq(step_size+1, nrow(asutused), step_size)) {
-  i_end <- min(i+step_size, nrow(asutused))
-  root$children <- c(root$children, list(make_node(
-    sprintf("Grupp %d-%d", i, i_end),
-    to_list(asutused[i:i_end,]))))
-}
 
 cat(toJSON(root), file="../data/flare_kov.json")
 
